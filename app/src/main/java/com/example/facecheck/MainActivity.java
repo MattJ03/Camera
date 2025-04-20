6a273ad741eb9381440c7c7d7615d34660b9a39a
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-20 18:31:23 +0100

    click listener for log in and start of self

--- app/src/main/java/com/example/facecheck/MainActivity.java
+++ app/src/main/java/com/example/facecheck/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.facecheck;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,8 +8,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.Manifest;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button logInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,26 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        username = findViewById(R.id.editTextUsernameTest);
+        password = findViewById(R.id.editTextTextPassword);
+        logInBtn = findViewById(R.id.buttonLogIn);
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().isEmpty() && !password.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter a username", Toast.LENGTH_LONG).show();
+                } else if(!username.getText().toString().isEmpty() && password.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter a password", Toast.LENGTH_LONG).show();
+                } else if(username.getText().toString().isEmpty() && password.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Enter credentials", Toast.LENGTH_LONG).show();
+                } else {
+                    username.setText("");
+                    password.setText("");
+                    Intent intent = new Intent(MainActivity.this, Self.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/facecheck/Self.java
@@ -0,0 +1,19 @@
+package com.example.facecheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.EditText;
+
+public class Self extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.self);
+
+
+    }
+}
