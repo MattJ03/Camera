4413240faf845112bed3889dbf06ad91b37bc031
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-20 21:21:49 +0100

    added permission check for oncrate

--- app/src/main/java/com/example/facecheck/Self.java
+++ app/src/main/java/com/example/facecheck/Self.java
@@ -1,19 +1,50 @@
 package com.example.facecheck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.EditText;
 
 public class Self extends AppCompatActivity {
 
+    Button captureBtn;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.self);
 
+        if(ActivityCompat.checkSelfPermission(Self.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_IMAGE_CAPTURE);
+        }
+
+
+        captureBtn = findViewById(R.id.button); // Make sure you have a button with this ID in self.xml
+
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureIntent();
+            }
+        });
+    }
+
+    private void takePictureIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            Log.d("SelfActivity", "Camera activity not found");
+        }
 
     }
 }
